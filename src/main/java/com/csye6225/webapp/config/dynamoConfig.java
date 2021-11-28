95e83bbf041d4dea907deec6b442fe69ced5d544
Olly96 <oliverrodrigues99@gmail.com>
2021-11-28 16:14:31 -0500

    test

--- src/main/java/com/csye6225/webapp/config/dynamoConfig.java
+++ src/main/java/com/csye6225/webapp/config/dynamoConfig.java
@@ -3,7 +3,7 @@ package com.csye6225.webapp.config;
 import com.amazonaws.auth.*;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 
-import org.springframework.cloud.aws.core.region.DefaultAwsRegionProviderChainDelegate;
+// import org.springframework.cloud.aws.core.region.DefaultAwsRegionProviderChainDelegate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
