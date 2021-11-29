079643f55a272da6975ac99547e703f8c90d30e7
Olly96 <oliverrodrigues99@gmail.com>
2021-11-28 19:03:13 -0500

    hk

--- src/main/java/com/csye6225/webapp/config/rdsConfig.java
+++ src/main/java/com/csye6225/webapp/config/rdsConfig.java
@@ -83,6 +83,8 @@ public class rdsConfig {
     transactionManager.setEntityManagerFactory(emf);
 
     return transactionManager;
+
+    }
     // @Primary
     // @Bean
     // public PlatformTransactionManager userTransactionManager() {
