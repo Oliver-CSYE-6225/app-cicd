324b9419048cc6e7e76adecd8386f7ff0ccaaab6
Olly96 <oliverrodrigues99@gmail.com>
2021-11-28 19:36:56 -0500

    test

--- src/main/java/com/csye6225/webapp/repository/ImageRepository.java
+++ src/main/java/com/csye6225/webapp/repository/ImageRepository.java
@@ -5,13 +5,14 @@ import com.csye6225.webapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, UUID> {
-
+    @Transactional(readOnly = true)
     @Query("SELECT i FROM Image i WHERE i.user_id = ?1")
     Optional<Image> findByUserId(UUID user_id);
 
